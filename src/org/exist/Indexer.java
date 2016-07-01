facfd5108fb60fee05655078794867fffdd4284a
Wolfgang Meier <wolfgang@exist-db.org>
2016-07-01 11:47:26 +0200

    Merge whitespace fixes by Leif

--- src/org/exist/Indexer.java
+++ src/org/exist/Indexer.java
@@ -114,7 +114,7 @@ public class Indexer extends Observable implements ContentHandler, LexicalHandle
     private String ignorePrefix = null;
     private ProgressIndicator progress;
 
-    private boolean suppressWSmixed = false;
+    protected boolean preserveWSmixed = false;
 
     protected int docSize = 0;
 
@@ -174,7 +174,7 @@ public class Indexer extends Observable implements ContentHandler, LexicalHandle
         Boolean temp;
         if ((temp = (Boolean) config
                 .getProperty(PROPERTY_PRESERVE_WS_MIXED_CONTENT)) != null) {
-            suppressWSmixed = temp;
+            preserveWSmixed = temp.booleanValue();
         }
     }
 
@@ -313,30 +313,29 @@ public class Indexer extends Observable implements ContentHandler, LexicalHandle
         //LOG.debug("elementCnt = " + childCnt.length);
     }
 
-    private void processText(final ElementImpl last) {
-        //keep for reference until sure that it's not required
-//        if (charBuf != null && charBuf.length() > 0) {
-//            // remove whitespace if the node has just a single text child,
-//            // keep whitespace for mixed content.
-//            final XMLString normalized;
-//            if ((charBuf.isWhitespaceOnly() && suppressWSmixed) || last.preserveSpace()) {
-//                normalized = charBuf;
-//            } else {
-//                if (last.getChildCount() == 0) {
-//                    normalized = charBuf.normalize(normalize);
-//                } else {
-//                    normalized = charBuf.isWhitespaceOnly() ? null : charBuf;
-//                }
-//            }
-//            if (normalized != null && normalized.length() > 0) {
-//                text.setData(normalized);
-//                text.setOwnerDocument(document);
-//                last.appendChildInternal(prevNode, text);
-//                if (!validate) storeText();
-//                setPrevious(text);
-//            }
-//            charBuf.reset();
-//        }
+    private void processText(ElementImpl last) {
+        // if (charBuf != null && charBuf.length() > 0) {
+        //    // remove whitespace if the node has just a single text child,
+        //    // keep whitespace for mixed content.
+        //     final XMLString normalized;
+        //     if ((charBuf.isWhitespaceOnly() && preserveWSmixed) || last.preserveSpace()) {
+        // 	normalized = charBuf;
+        //     } else {
+        // 	if (last.getChildCount() == 0) {
+        //            normalized = charBuf.normalize(normalize);
+        // 	} else {
+        // 	    normalized = charBuf.isWhitespaceOnly() ? null : charBuf;
+        // 	}
+        //     }
+        //     if (normalized != null && normalized.length() > 0) {
+        // 	text.setData(normalized);
+        // 	text.setOwnerDocument(document);
+        // 	last.appendChildInternal(prevNode, text);
+        // 	if (!validate) storeText();
+        // 	setPrevious(text);
+        //     }
+        //     charBuf.reset();
+        // }
 
         //from startElement method
         if (charBuf != null && charBuf.length() > 0) {
@@ -344,24 +343,45 @@ public class Indexer extends Observable implements ContentHandler, LexicalHandle
             if (charBuf.isWhitespaceOnly()) {
                 if (last.preserveSpace()) {
                     normalized = charBuf;
-                } else if (suppressWSmixed) {
+                } else if (last.getChildCount() == 0) {
+                    //normalized = charBuf.normalize(XMLString.COLLAPSE_WS);
+                    normalized = charBuf.normalize(normalize);
+                } else if (preserveWSmixed) {
                     if (!(last.getChildCount() == 0 && (normalize & XMLString.SUPPRESS_LEADING_WS) != 0)) {
                         normalized = charBuf;
                     }
+                } else {
+                    normalized = charBuf.normalize(normalize);
                 }
             } else {
-                // mixed element content: don't normalize the text node,
-                // just check if there is any text at all
-                normalized = charBuf;
+                //normalized = charBuf;
+                if (last.preserveSpace()) {
+                    normalized = charBuf;
+                } else if (last.getChildCount() == 0) {
+                    normalized = charBuf.normalize(normalize);
+                } else {
+                    // mixed element content: don't normalize the text node,
+                    // just check if there is any text at all
+                    if (preserveWSmixed) {
+                        normalized = charBuf;
+                    } else {
+                        if ((normalize & XMLString.SUPPRESS_LEADING_WS) != 0) {
+                            normalized = charBuf.normalize(XMLString.SUPPRESS_LEADING_WS | XMLString.COLLAPSE_WS);
+                        } else if ((normalize & XMLString.SUPPRESS_TRAILING_WS) != 0) {
+                            normalized = charBuf.normalize(XMLString.SUPPRESS_TRAILING_WS | XMLString.COLLAPSE_WS);
+                        } else {
+                            //normalized = charBuf.normalize(XMLString.COLLAPSE_WS);
+                            normalized = charBuf.normalize(normalize);
+                        }
+                    }
+                }
             }
 
             if (normalized != null) {
                 text.setData(normalized);
                 text.setOwnerDocument(document);
                 last.appendChildInternal(prevNode, text);
-                if (!validate) {
-                    storeText();
-                }
+                if (!validate) storeText();
                 setPrevious(text);
             }
             charBuf.reset();
